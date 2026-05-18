76f15b916b3191280dfee34587fd64f43dc6bd79
Andrea Gutiérrez Blanco <Andrea.Gutierrez0724@jala.university>
2026-05-17 23:41:10 -0400

    feat: HLTC-18 automatizar prueba actualizar perfil de usuario

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from services.github_issues_api import GitHubIssuesAPI
+from services.github_user_api import GitHubUserAPI
 from data.issue_data import CREATE_ISSUE_PAYLOAD
 
 
@@ -7,12 +8,15 @@ from data.issue_data import CREATE_ISSUE_PAYLOAD
 def github_api():
     return GitHubIssuesAPI()
 
+@pytest.fixture
+def github_user_api():
+    return GitHubUserAPI()
 
 @pytest.fixture
 def issue(github_api):
     response = github_api.create_issue(CREATE_ISSUE_PAYLOAD)
     issue_number = response.json()["number"]
-
+    
     yield issue_number
-
+    
     github_api.close_issue(issue_number)
---
+++ tests/users/test_hltc18_update_profile.py
@@ -0,0 +1,21 @@
+from jsonschema import validate
+from data.user_data import UPDATE_PROFILE_PAYLOAD
+from utils.schemas import UPDATE_PROFILE_SCHEMA
+
+def test_should_update_authenticated_user_profile_successfully(github_user_api):
+    payload = UPDATE_PROFILE_PAYLOAD
+
+    response = github_user_api.update_profile(payload)
+
+    assert response.status_code in [200, 204]
+
+    if response.status_code == 200:
+        body = response.json()
+
+        assert body["bio"] == payload["bio"]
+        assert body["location"] == payload["location"]
+
+        validate(instance=body, schema=UPDATE_PROFILE_SCHEMA)
+
+        assert body["login"] is not None
+        assert isinstance(body["id"], int)
